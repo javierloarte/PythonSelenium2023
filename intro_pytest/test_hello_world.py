6c513ca6187a0921ab827f5c56e427cbb86131ca
javierloarte <jall87@hotmail.com>
2023-04-25 20:58:02 -0500

    Initial commit

--- intro_pytest/test_hello_world.py
+++ intro_pytest/test_hello_world.py
@@ -1,6 +1,11 @@
 def test_hello_world():
     print("This is a test case")
+    result = suma(5,4)
+    assert result == 8, "Suma de 5 + 4 debe regresar 9"
 
+def suma (a, b)
+    return a + b
 
 def test_second_hello_world():
     print("This is a test case!")
+
