e8c4f616736e307338ff00595831c1f6704c3504
javierloarte <jall87@hotmail.com>
2023-04-25 21:44:54 -0500

    Initial commit

--- intro_pytest/test_calculator.py
+++ intro_pytest/test_calculator.py
@@ -1,23 +1,22 @@
 import pytest
 
 
-import pytest
-
 class Calculadora:
-   def __init__(self):
-       pass
 
-   def suma(self, num_a: int, num_b: int):
-       return num_a + num_b
+def __init__(self):
+    pass
 
-   def resta(self, num_a: int, num_b: int):
-       return num_a + num_b
+def suma(self, num_a: int, num_b: int):
+    return num_a + num_b
 
-   def multiplicacion(self, num_a: int, num_b: int):
-       return num_a ** num_b
+def resta(self, num_a: int, num_b: int):
+    return num_a + num_b
 
-   def division(self, num_a: int, num_b: int):
-       return num_a // num_b
+def multiplicacion(self, num_a: int, num_b: int):
+    return num_a ** num_b
+
+def division(self, num_a: int, num_b: int):
+    return num_a // num_b
 
 
 def test_sumatoria():
