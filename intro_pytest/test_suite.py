c1eb31a35a1663738f13511b066b78abc7cf2037
javierloarte <jall87@hotmail.com>
2023-04-26 21:11:58 -0500

    Initial commit

--- intro_pytest/test_suite.py
+++ intro_pytest/test_suite.py
@@ -18,31 +18,6 @@ class TestLogin:
     def test_invalid_login(self):
         print("Invalid login Test")
 
-    Luis
-    M
-    Rivas
-    Zepeda
-    21: 10
-
-
-class TestLogin:
-    @classmethod
-    def setup_class(cls):
-        print("*" * 80)
-        print(f"CLASS SETUP - Crear usuarios con llamadas a DB")
-        print("*" * 80)
-
-    def setup_method(self):
-        print("*" * 80)
-        print(f"METHOD SETUP - Abrir un nuevo navegador")
-        print("*" * 80)
-
-    def test_login(self):
-        print("Login test")
-
-    def test_invalid_logic(self):
-        print("Invalid login test")
-
     def teardown_method(self):
         print("*" * 80)
         print(f"TEARDOWN SETUP - Cerrar el navegador")
