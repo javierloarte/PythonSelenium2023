1a5d46c8761887714bfb6bcc0db83cd643d56d34
javierloarte <jall87@hotmail.com>
2023-04-26 20:40:31 -0500

    Initial commit

--- intro_pytest/test_calculadora.py
+++ intro_pytest/test_calculadora.py
@@ -17,20 +17,19 @@ class Calculadora:
         return num_a // num_b
 
 
-def test_suma_valid_input():
-    Calc = Calculadora()
-    result = Calc.suma(2, 3)
-    assert result == 5, "el resultado es 5"
-
+## ///////////////////////////////////////////////////////////////////////
+## Validacion
+## ///////////////////////////////////////////////////////////////////////
 
 def test_sumando():
-    assert suma("ok", 10, 3) == 12, "La suma es incorrecta"
+    calc = Calculadora()
+    assert calc.suma( 10, 3) == 13, "La suma es incorrecta"
 
 def test_restando():
-    assert resta("ok", 9, 3) == 4, "La resta es incorrecta"
+    assert resta( 9, 3) == 4, "La resta es incorrecta"
 
 def test_multiplicando():
-    assert resta("ok", 9, 2) == 20, "La muliplicacion es incorrecta"
+    assert resta( 9, 2) == 20, "La muliplicacion es incorrecta"
 
 def test_dividiendo():
-    assert resta("ok", 9, 3) == 2, "La division es incorrecta"
+    assert resta( 9, 3) == 2, "La division es incorrecta"
