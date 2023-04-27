c15f825ba225b97dd34ff2880f2ada4d31b5cd4c
javierloarte <jall87@hotmail.com>
2023-04-26 21:42:31 -0500

    Initial commit

--- intro_pytest/test_cuenta_bancaria.py
+++ intro_pytest/test_cuenta_bancaria.py
@@ -1,3 +1,5 @@
+import pytest
+
 
 class CuentaBancaria:
     def __init__(self, titular, saldo):
@@ -9,10 +11,26 @@ class CuentaBancaria:
         if cantidad > self.saldo:
             raise ValueError("No tienes suficiente saldo")
         self.saldo -= cantidad
-    def consultar_saldo(self):
 
-#*******************************************************
-# Validacion
-# *******************************************************
-def test_cuentaBancaria():
-    print("test cuenta bancaria")
+    def consultar_saldo(self):
+        pass
+
+
+## *******************************************************
+## Validacion
+## *******************************************************
+
+    @classmethod
+    def setup_class(cls):
+        print("*" * 80)
+        print(f"CLASS SETUP - Crear usuario con la llamada a la BD")
+        print("*" * 80)
+
+    def test_cuentaBancaria():
+        print("test cuenta bancaria")
+
+    @classmethod
+    def teardown_class(cls):
+        print("*" * 80)
+        print(f"TEARDOWN SETUP - limpiar usuario con la llamada a la BD")
+        print("*" * 80)
