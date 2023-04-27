840662040b3d792a00b345ad01bbe2809de75666
javierloarte <jall87@hotmail.com>
2023-04-26 20:19:29 -0500

    Initial commit

--- intro_pytest/test_calculator.py
+++ intro_pytest/test_calculator.py
@@ -3,8 +3,8 @@ import pytest
 
 class Calculadora:
 
-    def __init__(self):
-        pass
+def __init__(self):
+    pass
 
 def suma(self, num_a: int, num_b: int):
     return num_a + num_b
@@ -22,7 +22,7 @@ def division(self, num_a: int, num_b: int):
 def test_sumatoria():
     calc = Calculadora()
     result = calc.suma(2, 3)
-    assert result == 4, "el resultado es 5"
+    assert result == 5, "el resultado es 5"
 
 def test_restar():
     result = resta("ok",2, 10)
