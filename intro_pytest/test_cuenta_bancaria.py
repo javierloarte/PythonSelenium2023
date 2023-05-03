bc4bbf94aac755c116f04bb5c3d3d3d6dd9be117
javierloarte <jall87@hotmail.com>
2023-05-02 23:32:20 -0500

    Update test_cuenta_bancaria.py

--- intro_pytest/test_cuenta_bancaria.py
+++ intro_pytest/test_cuenta_bancaria.py
@@ -1,33 +1,54 @@
 import pytest
 
-
 class CuentaBancaria:
     def __init__(self, titular, saldo):
         self.titular = titular
         self.saldo = saldo
+
     def depositar(self, cantidad):
         self.saldo += cantidad
+
+
     def retirar(self, cantidad):
         if cantidad > self.saldo:
             raise ValueError("No tienes suficiente saldo")
         self.saldo -= cantidad
 
     def consultar_saldo(self):
-        pass
-
+        return self.saldo
 
 ## *******************************************************
 ## Validacion
 ## *******************************************************
+
+class Test_CuentaBancaria:
 
     @classmethod
     def setup_class(cls):
         print("*" * 80)
         print(f"CLASS SETUP - Crear usuario con la llamada a la BD")
         print("*" * 80)
+    def setup_method(self):
+        print("*" * 80)
+        print(f"METHOD SETUP - Abrir un nuevo navegador")
+        print("*" * 80)
+        self.cuenta = CuentaBancaria("Pedro", 100)
+        print(self.cuenta.titular)
+        print(self.cuenta.saldo)
 
-    def test_cuentaBancaria():
-        print("test cuenta bancaria")
+    def test_depositar(self):
+        self.cuenta.depositar(20)
+        assert self.cuenta.consultar_saldo() == 120, "Prueba Incorrecta"
+        print(self.cuenta.saldo)
+
+    def test_retirar(self):
+        self.cuenta.retirar(40)
+        assert self.cuenta.consultar_saldo() == 60, "Prueba Incorrecta"
+
+    def teardown_method(self):
+        print("*" * 80)
+        print(f"TEARDOWN SETUP - Cerrar el navegador")
+        print("*" * 80)
 
     @classmethod
     def teardown_class(cls):
