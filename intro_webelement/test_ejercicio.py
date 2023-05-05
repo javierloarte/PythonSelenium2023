bda851f48ec330d283634c61f11a0926f4f3a305
javierloarte <jall87@hotmail.com>
2023-05-04 22:22:27 -0500

    Initial commit

--- intro_webelement/test_ejercicio.py
+++ intro_webelement/test_ejercicio.py
@@ -9,13 +9,16 @@ url = "https://laboratorio.qaminds.com/"
 
 class TestLandingPage:
 
-    def __init__(self):
-        self.driver = None
-
     def setup_method(self):
         self.driver = webdriver.Firefox(service=firefox_service)
         self.driver.maximize_window()
         self.driver.get(url)
+
+    def test_course_button(self):
+        time.sleep(3)
+        element = self.driver.find_element(By.XPATH, "//a[normalize-space()='LABORATORIO']")
+        assert element.is_displayed(),"Elemento de tiene que ser visisble"
+
 
 
     def teardown_method(self):
