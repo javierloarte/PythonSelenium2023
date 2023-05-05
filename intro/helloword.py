42107800da42fa73943022292fbc406e7350b22b
javierloarte <jall87@hotmail.com>
2023-05-04 21:20:42 -0500

    Initial commit

--- intro/helloword.py
+++ intro/helloword.py
@@ -1,6 +1,7 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 
 chrome_driver_path = "./drivers/geckodriver.exe"
 url = "https://qamindslab.com/"
@@ -9,6 +10,7 @@ service = Service(chrome_driver_path)
 driver = webdriver.Firefox(service=service)
 
 driver.get(url)
+driver.find_element(By.XPATH,"")
 time.sleep(1)
 driver.quit()
 
