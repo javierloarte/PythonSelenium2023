0aa9693849704e523e22bd2d1b40dc6c7c6da71d
javierloarte <jall87@hotmail.com>
2023-05-04 22:03:36 -0500

    Initial commit

--- intro_webelement/test_ejercicio_01.py
+++ intro_webelement/test_ejercicio_01.py
@@ -4,24 +4,20 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
 chrome_driver_path = "./drivers/geckodriver.exe"
-service = Service(chrome_driver_path)
+firefox_service = Service(chrome_driver_path)
 url = "https://laboratorio.qaminds.com/"
 
 
-class test_busquedaiphone:
+class ejercicioUno:
 
-    def setup_method(self):
-        self.driver = webdriver.Firefox(service=service)
+    def __init__(self):
+        self.driver = None
+
+    def open_web(self):
+        self.driver = webdriver.Firefox(service=firefox_service)
         self.driver.maximize_window()
         self.driver.get(url)
 
 
-
-    def test_search_iphone(self):
-        time.sleep(3)
-        element = self.driver.find_element(By.XPATH, "//a[normalize-space()='LABORATORIO']")
-        assert element.is_displayed(),"Elemento de tiene que ser visisble"
-
-
-    def teardown_method(self):
+    def close_web(self):
         self.driver.quit()
