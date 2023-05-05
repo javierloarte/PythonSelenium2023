80c0ca2c7f4e44df2f845dd7a4ef69cd1fe07b72
javierloarte <jall87@hotmail.com>
2023-05-04 21:39:38 -0500

    Initial commit

--- intro_webelement/test_qamindlab.py
+++ intro_webelement/test_qamindlab.py
@@ -4,14 +4,14 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
 chrome_driver_path = "./drivers/geckodriver.exe"
-chrome_service = Service(chrome_driver_path)
+service = Service(chrome_driver_path)
 url = "https://qamindslab.com/"
 
 
 class TestLandingPage:
 
     def setup_method(self):
-        self.driver = webdriver.Firefox(service=chrome_service)
+        self.driver = webdriver.Firefox(service=service)
         self.driver.maximize_window()
         self.driver.get(url)
 
