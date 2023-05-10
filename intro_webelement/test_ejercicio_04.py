53fcf3fdd691a96bb44cf1e21e8becb6ea088a1a
javierloarte <jall87@hotmail.com>
2023-05-09 21:59:57 -0500

    Initial commit

---
+++ intro_webelement/test_ejercicio_04.py
@@ -0,0 +1,31 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+
+chrome_driver_path = "./drivers/geckodriver.exe"
+firefox_service = Service(chrome_driver_path)
+url = "https://laboratorio.qaminds.com/"
+
+
+class TestEjercicio04:
+
+    def setup_method(self):
+        self.driver = webdriver.Firefox(service=firefox_service)
+        self.driver.maximize_window()
+        self.driver.get(url)
+
+    def test_buscaLaptop(self):
+
+        # Opcion Laptops
+        time.sleep(3)
+        menu_lista = self.driver.find_element(By.XPATH, "//a[normalize-space()='Laptops & Notebooks']")
+        element = self.driver.find_element(By.XPATH, "//a[normalize-space()='Windows (0)']")
+        element.click()
+
+
+
+
+    def teardown_method(self):
+        self.driver.quit()
