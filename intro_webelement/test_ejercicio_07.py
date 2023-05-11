b2a78f3971af3c40d896e55a13da4634bdafad97
javierloarte <jall87@hotmail.com>
2023-05-10 21:16:54 -0500

    Initial commit

---
+++ intro_webelement/test_ejercicio_07.py
@@ -0,0 +1,34 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+
+chrome_driver_path = "./drivers/geckodriver.exe"
+firefox_service = Service(chrome_driver_path)
+url = "https://demoqa.com/select-menu"
+
+
+class TestEjercicio04:
+
+    def setup_method(self):
+        self.driver = webdriver.Firefox(service=firefox_service)
+        self.driver.maximize_window()
+        self.driver.get(url)
+
+    def test_Standard_multi_select(self):
+        # Opcion Laptops / Windows
+        time.sleep(2)
+        elementlista = self.driver.find_element(By.ID, "cars")
+        select = Select(elementlista)
+        time.sleep(1)
+        select.select_by_visible_text("Volvo")
+        time.sleep(1)
+        select.select_by_visible_text("Audi")
+        assert "Volvo" in select
+
+
+
+
+    def teardown_method(self):
+        self.driver.quit()
