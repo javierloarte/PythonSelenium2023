4a8d92c740d6909b8f22d1a656338e78d34e0b6e
javierloarte <jall87@hotmail.com>
2023-05-18 21:02:37 -0500

    Update practica_01.py

--- proyecto_practico_intermedio/practica_01.py
+++ proyecto_practico_intermedio/practica_01.py
@@ -2,13 +2,16 @@ from selenium.webdriver.common.by import By
 
 from factory.webdriver_factory import get_driver
 
-url = "https://laboratorio.qaminds.com/"
+URL = "https://laboratorio.qaminds.com/"
 
 class practica:
 
+    def __init__(self):
+        self.driver = None
+
     def setup_method(self):
         self.driver = get_driver()
-        self.driver.get(url)
+        self.driver.get(URL)
 
     def test_search_display(self):
 
