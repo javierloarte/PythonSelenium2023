0bc61a3e1279da8855378d5c8a3f8705d077f207
javierloarte <jall87@hotmail.com>
2023-05-18 20:57:00 -0500

    Initial commit

---
+++ proyecto_practico_intermedio/practica01.py
@@ -0,0 +1,25 @@
+from selenium.webdriver.common.by import By
+
+from factory.webdriver_factory import get_driver
+
+url = "https://laboratorio.qaminds.com/"
+
+class practica01:
+
+    def __init__(self):
+        self.driver = None
+
+    def setup_method(self):
+        self.driver = get_driver()
+        self.driver.get(url)
+
+    def test_search_display(self):
+
+        # Escribir Iphone
+        search_input = self.driver.find_element(By.NAME, "search")
+        assert search_input.is_displayed() and search_input.is_enabled(), "El campo de busqueda tiene que estar visible y habilitado"
+        search_input.send_keys("Iphone")
+
+
+    def teardown_method(self):
+        self.driver.quit()
